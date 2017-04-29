f961f6bae47d0693d1ecd02e96d2a93bcf2eb439
CarefulNow <danny25106@gmail.com>
2017-04-29 08:43:03 +0100

    Fixed tides heights issue

--- Ver2/web/assets/js/pp/worldtides.js
+++ Ver2/web/assets/js/pp/worldtides.js
@@ -2,7 +2,7 @@ var WorldTides = function(){
 
     return {
         fetch: function(_f){
-            var _apiUrl= "https://www.worldtides.info/api?extremes&_PH_&start=_start_&key=d7f67d32-af18-4930-833e-ff638fe826bf";
+            var _apiUrl= "https://www.worldtides.info/api?extremes&_PH_&datum=lat&start=_start_&key=d7f67d32-af18-4930-833e-ff638fe826bf";
             var updatedUrl = _apiUrl.replace('_PH_',"lon="+loc.getLat()+"&lat="+loc.getLon()); //inverting these  wtf??!!
             var tideNow = datePicker.getDate().valueOf() / 1000;
             console.log("Tide Search Time %s",parseInt(tideNow));
