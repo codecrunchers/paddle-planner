54430bab1e127d297be842dfb6c01afa1548291f
Alan <alan@codecrunchers.ie>
2017-08-20 18:58:44 +0100

    Grr sls & webpack

--- app/api/weather.js
+++ app/api/weather.js
@@ -6,8 +6,7 @@ module.exports.weather = (event, context, callback) => {
     const latTd = event.pathParameters.lat;
     const lonTd = event.pathParameters.long;
     const date = parseInt(event.pathParameters.utc_timestamp);
-
-    //Returns and templates!! todays weather
+    //Returns today's weather
     var weatherNow='http://api.openweathermap.org/data/2.5/weather?_PH_&appid=b02e22a57d807e57968f7817363f59b2';
     //Return a list object with 40 entries, = 5*8*3 or 5 days by 24hrs at 3hour intervals
     var weatherFcast='http://api.openweathermap.org/data/2.5/forecast?_PH_&appid=b02e22a57d807e57968f7817363f59b2';
--- tf/main.tf
+++ tf/main.tf
@@ -7,14 +7,6 @@
 # vpc_details   = "${var.vpc_details_paddle_planner}"
 # subnet_cidrs  = "${var.subnet_cidrs_paddle_planner}"
 #}
-
-module "s3_paddle_planner_http" {
-  source      = "modules/s3_http_server/"
-  bucket_name = "paddle-planner.com"
-  acl         = "public-read"
-  versioning  = true
-}
-
 module "s3_paddle_planner_http_www" {
   source      = "modules/s3_http_server/"
   bucket_name = "www.paddle-planner.com"
