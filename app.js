03461732fa92ad21ef4d39ef05ea5923b67737ae
Alan Ryan <alan.ryan@gmail.com>
2018-11-03 15:39:40 +0000

    Add First Test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const healthRouter  = require('./routes/health');
 
 var app = express();
 
@@ -21,8 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/health', healthRouter);
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
---
+++ test/routes.js
@@ -0,0 +1,22 @@
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+
+const test = require('tape');
+
+test('GET /health', t => {
+  api
+  .get('/health')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      t.fail(err);
+      t.end();
+    } else {
+      t.ok(res.body, 'It should have a response body');
+      t.equals(res.body.healthy, true, 'It should return a healthy parameter and it should be true');
+      t.end();
+    }
+  });
+});
