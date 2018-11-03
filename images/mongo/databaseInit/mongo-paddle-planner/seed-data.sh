34549dd2e07bae2f937c8dce3bebd7668b32cb3d
Alan Ryan <alan.ryan@gmail.com>
2018-11-03 23:32:15 +0000

    Remove Secrets

--- images/mongo/databaseInit/mongo-paddle-planner/seed-data.sh
+++ images/mongo/databaseInit/mongo-paddle-planner/seed-data.sh
@@ -3,17 +3,17 @@
 echo 'Creating application user and db'
 
 mongo ${APP_MONGO_DB} \
-        --host localhost \
-        --port ${MONGO_PORT} \
-        -u ${MONGO_ROOT_USER} \
-        -p ${MONGO_ROOT_PASS} \
-        --authenticationDatabase admin \
-        --eval "db.createUser({user: '${APP_MONGO_USER}', pwd: '${APP_MONGO_PASS}', roles:[{role:'dbOwner', db: '${APP_MONGO_DB}'}]);" && \
-          mongo ${APP_MONGO_DB} \
-            --host localhost \
-            --port 27017 \
-            -u root \
-            -p example \
-            --authenticationDatabase paddle-planner \
-            --eval "db.users.insertOne({username: '${APP_DEFAULT_USER}', password: '${APP_DEFAULT_PASS}')"
+  --host ${APP_MONGO_HOST} \
+  --port ${MONGO_PORT} \
+  -u ${MONGO_ROOT_USER} \
+  -p ${MONGO_ROOT_PASS} \
+  --authenticationDatabase admin \
+  --eval "db.createUser({user: '${APP_MONGO_USER}', pwd: '${APP_MONGO_PASS}', roles:[ {role:'dbOwner', db: '${APP_MONGO_DB}'} ]} );" && \
+      mongo "${APP_MONGO_DB}" \
+      --host ${APP_MONGO_HOST} \
+      --port ${MONGO_PORT} \
+      -u ${APP_MONGO_USER} \
+      -p ${APP_MONGO_PASS} \
+      --authenticationDatabase ${APP_MONGO_DB} \
+      --eval "db.users.insertOne({username: '${APP_DEFAULT_USER}', password: '${APP_DEFAULT_PASS}'});"
 
