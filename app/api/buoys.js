d3d539b4c944f230f093e6d9bfeb439184a49232
Alan Ryan <alan.ryan@gmail.com>
2019-07-02 00:43:34 +0100

    working call

--- app/api/buoys.js
+++ app/api/buoys.js
@@ -10,23 +10,19 @@ const LOG_DATA = process.env.LOG_DATA || false
  */
 exports.getBuoyOffline =  async (_request, reply, cb)=> { 
   logger.log({level: "info", message: "DEBUG MODE"});
-  return new Promise( resolve => {
-    resolve(cb("HEAD,HEAD1,HEADER2\r\n1,2,B\r\n2,3,A"))
-  })
+  cb(null, "HEAD,HEAD1,HEADER2\r\n1,2,B\r\n2,3,A")
 }
 
-exports.getBuoy = (_request, cb) => {
+exports.getBuoy = async (_request, cb) => {
   try {
     logger.log({level: "info", message: "LIVE MODE"});
     const buoyId  = _request.params.buoyid;
     const updatedUrl = `https://www.met.ie/forecasts/marine-inland-lakes/buoys/download/${buoyId}`;
     logger.log({level: "debug", message: "Fetching URI " , updatedUrl});
-    return new Promise( resolve => {
-       request.get(updatedUrl, (e,r) => {
-        logger.log({level: "debug", message: "IN request CB", e, r});
-        resolve( cb(e,r) )
-      })
-    });
+    request.get(updatedUrl, (e,r) => {
+      logger.log({level: "debug", message: "IN request CB", e, r});
+      cb(e,r)
+    });    
   } catch (err) {
     logger.log({level: "error", message: "!!" , err});
   }
--- app/api/decorators.js
+++ app/api/decorators.js
@@ -5,19 +5,16 @@ const {csvToJSON, logBuoyData} = require('../utils/format')
 exports.elasticDecorator = async (request, reply) => {
   try {
 
-    await buoys.getBuoy(request, (e, r) => { 
-        logger.info("In CB");
-        if(e) {
-          reply.code(500);
-        }else {
-          reply.code(200).send(r.body)
-        }
-      }).then( () => 
-        {
-          console.log("Done")
-          return;
-        }
-      )
+  return buoys.getBuoy(request, (e, r) => { 
+      logger.info("In CB");
+      if(e) {
+        reply.code(500);
+      }else {
+        reply.code(200).type("application/json").send(r.body)
+      }
+    }).then( ()  =>  { return } )
+
+    return ;
   } catch (err) {
     console.log(err)
   }
